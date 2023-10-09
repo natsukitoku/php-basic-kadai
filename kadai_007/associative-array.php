ce8f6486a8111d269310e506dd69c6179b7b626b
natsukitoku <marron0831@outlook.jp>
2023-10-09 17:37:05 +0900

    kadai_007を追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='UTF-8'>
+    <title></title>
+  </head>
+
+  <body>
+    <p>
+         <?php
+         $vegitable = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+         print_r($vegitable);
+         ?>
+    </p>
+  </body>
+</html>
